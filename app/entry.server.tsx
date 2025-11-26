e1e8e14ea92c42558935626d11cddbf31cb99c04
xianyang <emluxy@gmail.com>
2025-11-26 17:18:50 +0800

    gst

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,12 +1,7 @@
-import { PassThrough } from "node:stream";
-
 import type { EntryContext } from "react-router";
-import { createReadableStreamFromReadable } from "@react-router/node";
 import { ServerRouter } from "react-router";
 import { isbot } from "isbot";
-import type { RenderToPipeableStreamOptions } from "react-dom/server";
 import {
-  renderToPipeableStream,
   renderToReadableStream,
 } from "react-dom/server";
 import { QueryClientProvider } from "@tanstack/react-query";
@@ -60,50 +55,5 @@ export default async function handleRequest(
   return new Response(stream, {
     status: hustonWeHaveAProblem ? 500 : responseStatusCode,
     headers: responseHeaders,
-  });
-
-  return new Promise((resolve, reject) => {
-    let shellRendered = false;
-    const userAgent = request.headers.get("user-agent");
-
-    const readyOption: keyof RenderToPipeableStreamOptions =
-      (userAgent && isbot(userAgent)) || routerContext.isSpaMode
-        ? "onAllReady"
-        : "onShellReady";
-
-    const { pipe, abort } = renderToPipeableStream(
-      <QueryClientProvider client={queryClient}>
-        <I18nextProvider i18n={instance}>
-          <ServerRouter context={routerContext} url={request.url} />
-        </I18nextProvider>
-      </QueryClientProvider>,
-      {
-        [readyOption]() {
-          shellRendered = true;
-          const body = new PassThrough();
-          const stream = createReadableStreamFromReadable(body);
-
-          responseHeaders.set("Content-Type", "text/html");
-
-          resolve(
-            new Response(stream, {
-              headers: responseHeaders,
-              status: responseStatusCode,
-            })
-          );
-
-          pipe(body);
-        },
-        onShellError(error: unknown) {
-          reject(error);
-        },
-        onError(error: unknown) {
-          responseStatusCode = 500;
-          if (shellRendered) console.error(error);
-        },
-      }
-    );
-
-    setTimeout(abort, streamTimeout + 1000);
   });
 }
